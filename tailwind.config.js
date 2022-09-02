2ebc3e207cf51fdf8e8cfecc8d628fea2fcd00ba
Ivan Radojevic <ivanradojevic.gm@gmail.com>
2022-09-02 08:19:32 +0200

    Add style to main recipe page

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      fontFamily:{
+        shand: "Shrikhand, serif",
+        opensans: "'Open Sans', sans-serif"
+      }
+    },
+  },
+  plugins: [],
+}
